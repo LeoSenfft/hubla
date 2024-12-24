8eaa5fcd306d785912d1e46b5fc0873378a37c35
leosenfft <leo.senfft@gmail.com>
2024-12-24 10:25:02 -0300

    Add Footer component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <footer className="bg-slate-800 text-center text-white mt-auto py-4">
+      <div className="container">Copyright 2024</div>
+    </footer>
+  );
+}
