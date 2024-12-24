b357802240838fcccc34bdd7a1ee218478255d57
leosenfft <leo.senfft@gmail.com>
2024-12-24 10:41:52 -0300

    Add tailwind merge

--- frontend/src/components/header/HeaderLink.tsx
+++ frontend/src/components/header/HeaderLink.tsx
@@ -1,13 +1,19 @@
 import Link, { type LinkProps } from "next/link";
 import React from "react";
 
+import { twMerge } from "tailwind-merge";
+
 interface HeaderLinkProps extends LinkProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export function HeaderLink({ children, ...props }: HeaderLinkProps) {
+export function HeaderLink({ children, className, ...props }: HeaderLinkProps) {
   return (
-    <Link {...props} className="">
+    <Link
+      {...props}
+      className={twMerge("px-4 hover:underline underline-offset-2", className)}
+    >
       {children}
     </Link>
   );
