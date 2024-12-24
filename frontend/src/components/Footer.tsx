7f1900b5ab5ea7019de0b8a78c9f67f852ccd2bb
leosenfft <leo.senfft@gmail.com>
2024-12-24 11:00:49 -0300

    Remove mt-auto

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Footer() {
   return (
-    <footer className="bg-slate-800 text-center text-white mt-auto py-4">
+    <footer className="bg-slate-800 text-center text-white py-4">
       <div className="container">Copyright 2024</div>
     </footer>
   );
