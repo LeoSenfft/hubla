0661f963e3c41a6e17cfeb691c843c69a52e2613
leosenfft <leo.senfft@gmail.com>
2024-12-26 17:26:00 -0300

    Add error behavior

--- frontend/src/app/hooks/sales.tsx
+++ frontend/src/app/hooks/sales.tsx
@@ -1,19 +1,35 @@
 import type { ISale } from "@/types/Sales";
 import React, { useEffect, useState } from "react";
 import axiosInstance from "@/lib/axios-instance";
+import { useToast } from "@/hooks/use-toast";
 
 export function useSales() {
+  const { toast } = useToast();
+
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [sales, setSales] = useState<Array<ISale>>([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function main() {
+      if (isLoading) {
+        return;
+      }
+
+      setError(null);
+
       try {
         setIsLoading(true);
         const resp = await axiosInstance.get("/");
 
         setSales(resp.data.data ?? []);
-      } catch (error) {
+      } catch (error: any) {
+        toast({
+          variant: "destructive",
+          title: "Algo deu errado.",
+          description: error.response.data.message,
+        });
+        setError(error.response.data.message);
         console.error(error);
       } finally {
         setIsLoading(false);
@@ -23,5 +39,5 @@ export function useSales() {
     main();
   }, []);
 
-  return { sales, setSales, isLoading };
+  return { sales, setSales, isLoading, error };
 }
--- frontend/src/app/hooks/users.tsx
+++ frontend/src/app/hooks/users.tsx
@@ -1,19 +1,34 @@
 import { useEffect, useState } from "react";
 import axiosInstance from "@/lib/axios-instance";
 import type { IUser } from "@/types/User";
+import { useToast } from "@/hooks/use-toast";
 
 export function useUsers() {
+  const { toast } = useToast();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [users, setUsers] = useState<Array<IUser>>([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function main() {
+      if (isLoading) {
+        return;
+      }
+
+      setError(null);
+
       try {
         setIsLoading(true);
         const resp = await axiosInstance.get("/users");
 
         setUsers(resp.data.data ?? []);
-      } catch (error) {
+      } catch (error: any) {
+        toast({
+          variant: "destructive",
+          title: "Algo deu errado.",
+          description: error.response.data.message,
+        });
+        setError(error.response.data.message);
         console.error(error);
       } finally {
         setIsLoading(false);
@@ -23,5 +38,5 @@ export function useUsers() {
     main();
   }, []);
 
-  return { users, setUsers, isLoading };
+  return { users, setUsers, isLoading, error };
 }
