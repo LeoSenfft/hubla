46d74f56eadb799f51963ca8bce0e766e2e34732
leosenfft <leo.senfft@gmail.com>
2024-12-26 10:09:57 -0300

    Add route GET all sales

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,28 @@ import express from "express";
 import multer from "multer";
 import multerConfig from "../config/multer";
 import { readFile } from "../services/sales-service";
+import { PrismaClient } from "@prisma/client";
 
 export const router = express.Router();
+const client = new PrismaClient();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
+router.get("/", async (req, res) => {
+  try {
+    const data = await client.sale.findMany({ include: { type: true } });
+
+    res.json({
+      status: 200,
+      data: data,
+      message: "Sucesso!",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.json({
+      status: 500,
+      message: error,
+    });
+  }
 });
 
 router.post("/", multer(multerConfig).single("file"), (req, res) => {
