469b39b76db01d92238b4118f954c8ccc4f69b14
leosenfft <leo.senfft@gmail.com>
2024-12-27 16:18:18 -0300

    Add new link to "log in" or "log out" in menu depending if user is logged or not

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,32 @@
-import Link from "next/link";
+"use client";
+
 import React from "react";
 import { HeaderLink } from "./header/HeaderLink";
+import { useAuth } from "@/hooks/useAuth";
 
 export function Header() {
+  const { signOut, currentUser } = useAuth();
+
+  function handleSignOut(e: React.MouseEvent) {
+    e.preventDefault();
+
+    signOut();
+  }
+
   return (
     <header className="py-6 bg-slate-600 text-white">
       <div className="container">
         <nav className="flex justify-center">
           <HeaderLink href={"/"}>Home</HeaderLink>
           <HeaderLink href={"/saldos"}>Saldos</HeaderLink>
+
+          {currentUser ? (
+            <HeaderLink href={"/login"} onClick={handleSignOut}>
+              Sair
+            </HeaderLink>
+          ) : (
+            <HeaderLink href={"/login"}>Entrar</HeaderLink>
+          )}
         </nav>
       </div>
     </header>
