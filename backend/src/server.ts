a277436d75528b8ef3003c6a8fa2f3103b994119
leosenfft <leo.senfft@gmail.com>
2024-12-27 16:08:15 -0300

    Separate routes and add some PrismaClient params to hide password from user routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,21 +3,33 @@ import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 import { router } from "./routes";
 import cors from "cors";
+import usersRouter from "./routes/users";
+import authRouter from "./routes/auth";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
 
-const client = new PrismaClient();
+export const client = new PrismaClient({
+  omit: {
+    user: {
+      password: true,
+    },
+  },
+});
 
 client
   .$connect()
   .then(() => console.log("Conectado ao PostgreSQL"))
   .catch((err) => console.error("Erro ao conectar ao PostgreSQL", err));
 
-app.use(router);
+app.use(cors());
+app.use(express.json());
+
+app.use("/", router);
+app.use("/auth", authRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
